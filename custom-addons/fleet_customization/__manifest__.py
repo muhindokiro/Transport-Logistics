2404677af53af8386fc289e7e1d931ae89c3a58d
muhindokiro <blaize1143@gmail.com>
2023-04-24 22:46:47 +0300

    create new module for fleet customization

---
+++ custom-addons/fleet_customization/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "Fleet Customization",
+    'summary': """This module will to run fleet customizations""",
+    'version': '15.0.1.0.0',
+    'description': """This module will add a record to for fleet module""",
+    'author': '@Muhindo-Kiro',
+    'category': 'Tools',
+    'depends': ['base','hr','contacts','fleet'],
+    'license': 'AGPL-3',
+    'data': [
+        'views/fleet_view.xml',
+        ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
