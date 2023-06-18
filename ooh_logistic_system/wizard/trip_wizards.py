a47727b2c3a23237fbcfb795fd78fa513c6737cc
Kadweka <husseinkatana17@gmail.com>
2023-06-18 18:31:02 +0300

    update changes

--- ooh_logistic_system/wizard/trip_wizards.py
+++ ooh_logistic_system/wizard/trip_wizards.py
@@ -15,26 +15,37 @@ class TripManager(models.Model):
     
     _name = "trip.management.vehicle"
     _description = "Transporter Management"
-    
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name', _('New')) == _('New'):
+            vals['name'] = self.env['ir.sequence'].next_by_code('vehicle.trip') or _('New')
+        res = super(TripManager, self).create(vals)
+        return res
     type = fields.Selection([
         ('internal', 'Internal'),
         ('external', 'External')], string='Transport Type', tracking=True)
+    name = fields.Char(string='Trip Reference', readonly=True)
 
     """FOR EXTERNAL TRIP DRIVERS PREFERENCES"""
-
+    state = fields.Selection([
+        ('Draft', 'Draft'),
+        ('External', 'External'),
+        ('Cancelled', 'Cancelled'),
+        ('Finished', 'Finished'),
+    ], default='Draft')
+    date = fields.Date(string="Departure Date")
     external_driver_name = fields.Char(string='Driver Name')
-    external_driver_mobile = fields.Char(string='Driver Name')
+    external_driver_mobile = fields.Char(string='Driver Mobile')
     external_truck_number = fields.Char(string='Vehicle Plate No.')
     external_turnboy_name = fields.Char(string='Turnboy Name')
-    external_turnboy_mobile = fields.Char(string='Driver Name')
+    external_turnboy_mobile = fields.Char(string='Turnboy Mobile')
     file_ids=fields.Many2one("open.file",string="Related File")
-
+    ref=fields.Char(string="File Ref",related="file_ids.name")
+    partner_id = fields.Many2one('res.partner',string="Client",related="file_ids.customer_id")
 
     """INTERNAL TRANSPORTERS"""
     internal_driver = fields.Many2one("hr.employee", string='Driver Name')
     internal_truck = fields.Many2one("fleet.vehicle", string='Vehicle Plate No.')
     internal_turnboy = fields.Many2one("res.partner", string='Turnboy Name')
-
-
-    def create_transporter(self):
-        print("HUSSEIN KADWEKA KATANA")
+    internal_turnboy_mobile = fields.Char(string='Turnboy Mobile')
