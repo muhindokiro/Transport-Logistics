ef220c09bf0fa2d910d5594eeb57e5a8a51a1046
muhindokiro <blaize1143@gmail.com>
2023-06-19 15:12:45 +0300

    add t810 entry view

--- ooh_logistic_system/__manifest__.py
+++ ooh_logistic_system/__manifest__.py
@@ -17,7 +17,8 @@
         'views/file.xml',
         'views/bond.xml',
         'views/shipping_line.xml',
-        'views/documents.xml'
+        'views/documents.xml',
+        'views/t810.xml'
         # 'wizard/trip_wizard_view.xml'
     ],
     'demo': [],
